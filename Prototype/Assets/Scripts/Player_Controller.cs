4b95480ee0c9d1fef7a43a3f2ee92a50480aa62d
Scott Williams <sw14k@my.fsu.edu>
2018-10-06 00:42:54 -0400

    Updated player_controller script to give better control of movement.

---
+++ Prototype/Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller: MonoBehaviour {
+
+	public float speed;		//Floating point variable to store the player's movement speed.
+	
+	private Rigidbody2D rb2d;   //Store a reference to the Rigidbody2D component required to use 2D Physics.
+    private Vector2 moveVelocity;
+		
+	// Use this for initialization
+	void Start () 
+	{
+
+		//Get and store a reference to the Rigidbody2D component so that we can access it.
+		rb2d = GetComponent<Rigidbody2D> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput.normalized * speed;
+	}
+
+    private void FixedUpdate()
+    {
+        rb2d.MovePosition(rb2d.position + moveVelocity * Time.fixedDeltaTime);
+    }
+
+}
