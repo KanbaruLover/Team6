a2bb70e08ee2ac82fa0eb285c5a3d0d44391afe8
Scott Williams <sw14k@my.fsu.edu>
2018-10-26 16:12:08 -0400

    Added boundaries on the map, temporary background music, and extended the map.

--- Prototype/Assets/Scripts/Player_Controller.cs
+++ Prototype/Assets/Scripts/Player_Controller.cs
@@ -8,9 +8,12 @@ public class Player_Controller: MonoBehaviour {
 	
 	private Rigidbody2D rb2d;   //Store a reference to the Rigidbody2D component required to use 2D Physics.
     private Vector2 moveVelocity;
-		
-	// Use this for initialization
-	void Start () 
+    private int minY = -3;
+    private int maxY = 1;
+
+
+    // Use this for initialization
+    void Start () 
 	{
 
 		//Get and store a reference to the Rigidbody2D component so that we can access it.
@@ -19,15 +22,19 @@ public class Player_Controller: MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () 
-	{
+    void Update () 
+    {
+        Mathf.Clamp(transform.position.y, minY, maxY);
         Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         moveVelocity = moveInput.normalized * speed;
+
 	}
 
     private void FixedUpdate()
     {
+
         rb2d.MovePosition(rb2d.position + moveVelocity * Time.fixedDeltaTime);
     }
+
 
 }
